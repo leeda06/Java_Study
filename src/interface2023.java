a621dca895ed992d7a1b92b6ba611be02efa1dc3
wjdwodhks35 <88700168+leeda06@users.noreply.github.com>
2023-03-20 14:05:22 +0900

    커밋

---
+++ src/interface2023.java
@@ -0,0 +1,15 @@
+/*
+    덕 타이핑   -   오리처럼 걷고 오리처럼 꽥꽥거리면 그것은 오리이다
+    -   타입을 중시하는사람   =>  X (Person은 Duck이 아니니깐)
+    -   행동을 중시하는 사람  =>  O  (Person이건 Duck이 하는 행동을 하니깐)
+        =   squak   :   행동을 중시  (step이있으면 Duck이다)
+ */
+
+interface Payable{
+    void pay();
+}
+public class interface2023 {
+    public static void main(String[] args){
+
+    }
+}
